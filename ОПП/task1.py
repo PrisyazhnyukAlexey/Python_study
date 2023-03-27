b81d85be447fad692da0d1f40a66ef5eb58c1e9a
Alexey Prisyazhnyuk <kaktack11197@mail.ru>
2023-03-27 22:42:31 +0300

    add all class

--- ОПП/task1.py
+++ ОПП/task1.py
@@ -3,16 +3,58 @@
 
 # print(dir(str))
 
-class Character:
-    gender = ''
-    name = ''
-    height = 0
-    weight = 0
-    hands = 2
-    weapom = []
+# class Character:
+#     gender = ''
+#     name = ''
+#     height = 0
+#     weight = 0
+#     hands = 2
+#     weapom = []
 
-peter = Character()
-bruce = Character()
-peter.weapom = 'gun'
-print(peter.weapom)
-print(bruce.weapom)
+# peter = Character()
+# bruce = Character()
+# peter.weapom = 'gun'
+# print(peter.weapom)
+# print(bruce.weapom)
+
+class Cow():
+    weight = ''
+    sound = 'my-my'
+    def eat(self, food):
+        self.weight -= food
+
+    def domilk(liter):
+        weight -= liter * 0.7
+
+class Goose():
+    weight = ''
+    sound = 'cri-cri'
+    def eat(self, food):
+        self.weight -= food
+
+class Sheep():
+    weight = ''
+    sound = 'be-be'
+    def eat(self, food):
+        self.weight -= food
+
+class Chicken():
+    weight = ''
+    sound = 'ko-ko'
+    def eat(self, food):
+        self.weight -= food
+
+class Goat():
+    weight = ''
+    sound = 'be-be-be'
+    def eat(self, food):
+        self.weight -= food
+
+    def domilk(liter):
+        weight -= liter * 0.7
+
+class Duck():
+    weight = ''
+    sound = 'cry-cry'
+    def eat(self, food):
+        self.weight -= food
