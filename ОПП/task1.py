54b102a6e87361c099c9f2d5a6804fd414d69777
Alexey Prisyazhnyuk <kaktack11197@mail.ru>
2023-03-28 22:32:47 +0300

    final file

--- ОПП/task1.py
+++ ОПП/task1.py
@@ -9,46 +9,46 @@ class Cow():
         self.name = name
 
     def eat(self, food):
-        self.weight -= food
+        self.weight += food    
 
     def domilk(liter):
-        weight -= liter * 0.7
+        weight -= liter * 0.7            
 
 class Goose():
     def __init__(self, name, weight, sound='cri-cri'):
         self.weight = weight
         self.sound = sound
         self.name = name
-        def eat(self, food):
-            self.weight -= food
+    def eat(self, food):
+            self.weight += food    
 
 class Sheep():
     def __init__(self, name, weight, sound='be-be'):
         self.weight = weight
         self.sound = sound
         self.name = name
-        def eat(self, food):
-            self.weight -= food
+    def eat(self, food):
+            self.weight += food    
 
 class Chicken():
-    def __init__(self, name, weight=0, sound='ko-ko', agg=0):
+    def __init__(self, name, weight, sound='ko-ko', agg=0):
         self.weight = weight
         self.sound = 'ko-ko'
         self.agg = agg
         self.name = name
-
+        
     def eat(self, food):
-        self.weight -= food
+        self.weight += food
     def takeagg(self, amount):
-        self.agg -= amount
+         self.agg -= amount
 
 class Goat():
     def __init__(self, name, weight, sound='be-be-be'):
         self.weight = weight
         self.sound = 'be-be-be'
         self.name = name
-        def eat(self, food):
-            self.weight -= food
+    def eat(self, food):
+        self.weight += food
 
     def domilk(liter):
         weight -= liter * 0.7
@@ -58,8 +58,8 @@ class Duck():
         self.weight = weight
         self.sound = sound
         self.name = name
-        def eat(self, food):
-            self.weight -= food
+    def eat(self, food):
+        self.weight += food
 
 kukareku = Chicken(name='Kukareku',agg=2,weight=12)
 koko = Chicken(name='koko',agg=20,weight=10)
@@ -72,17 +72,15 @@ Horn = Goat(name='Horn',weight=35)
 Hooves = Goat(name='Hooves',weight=34)
 Kruakva = Duck(name='Kruakva',weight=42)
 
+Grey.eat(10)
 
+# Общий вес
+print(f'Общий вес всех животных: {kukareku.weight + koko.weight + Grey.weight + White.weight + Manka.weight + Barashek.weight + Kudriavy.weight + Horn.weight + Hooves.weight + Kruakva.weight}')
 
-
-print(kukareku.weight + koko.weight + Grey.weight + White.weight + Manka.weight + Barashek.weight + Kudriavy.weight + Horn.weight + Hooves.weight + Kruakva.weight)
-
-
-# listtest = []
-# listtest.append(x)
-# listtest.append(y)
-# listtest.append(z)
-# listtest.sort()
-# print(listtest)
-
-# print(listtest[-1])
+#Самое тяжелое животное
+allweight = {kukareku.name: kukareku.weight,  koko.name: koko.weight, Grey.name: Grey.weight, White.name: White.weight, Manka.name: Manka.weight, Barashek.name: Barashek.weight, Kudriavy.name: Kudriavy.weight, Horn.name: Horn.weight, Hooves.name: Hooves.weight, Kruakva.name: Kruakva.weight}
+print(allweight)
+max_weight = max(allweight.values())
+final_dict = {k:v for k, v in allweight.items() if v == max_weight}
+list_dict = list(final_dict.keys())
+print(f'Самое тяжелое животное: {list_dict[0]}')
