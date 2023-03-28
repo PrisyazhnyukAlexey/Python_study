ac03085060db54e8468b50188b96504833c226e3
Alexey Prisyazhnyuk <lehan1819@mail.ru>
2023-03-28 19:11:06 +0300

    do task1 and task2

--- ОПП/task1.py
+++ ОПП/task1.py
@@ -1,26 +1,12 @@
 # python - объекто - орентированный язык 
 # python - и в тоже время  процедурный язык
 
-# print(dir(str))
-
-# class Character:
-#     gender = ''
-#     name = ''
-#     height = 0
-#     weight = 0
-#     hands = 2
-#     weapom = []
-
-# peter = Character()
-# bruce = Character()
-# peter.weapom = 'gun'
-# print(peter.weapom)
-# print(bruce.weapom)
 
 class Cow():
-    def __init__(self, weight, sound='my-my'):
+    def __init__(self, name, weight, sound='my-my'):
         self.weight = weight
         self.sound = sound
+        self.name = name
 
     def eat(self, food):
         self.weight -= food
@@ -29,44 +15,74 @@ class Cow():
         weight -= liter * 0.7
 
 class Goose():
-    weight = ''
-    sound = 'cri-cri'
-    def eat(self, food):
-        self.weight -= food
+    def __init__(self, name, weight, sound='cri-cri'):
+        self.weight = weight
+        self.sound = sound
+        self.name = name
+        def eat(self, food):
+            self.weight -= food
 
 class Sheep():
-    weight = ''
-    sound = 'be-be'
-    def eat(self, food):
-        self.weight -= food
+    def __init__(self, name, weight, sound='be-be'):
+        self.weight = weight
+        self.sound = sound
+        self.name = name
+        def eat(self, food):
+            self.weight -= food
 
 class Chicken():
-    def __init__(self, weight=0, sound='ko-ko', agg=0):
-        self.weight = ''
+    def __init__(self, name, weight=0, sound='ko-ko', agg=0):
+        self.weight = weight
         self.sound = 'ko-ko'
         self.agg = agg
-
+        self.name = name
 
     def eat(self, food):
         self.weight -= food
+    def takeagg(self, amount):
+        self.agg -= amount
 
 class Goat():
-    weight = ''
-    sound = 'be-be-be'
-    def eat(self, food):
-        self.weight -= food
+    def __init__(self, name, weight, sound='be-be-be'):
+        self.weight = weight
+        self.sound = 'be-be-be'
+        self.name = name
+        def eat(self, food):
+            self.weight -= food
 
     def domilk(liter):
         weight -= liter * 0.7
 
 class Duck():
-    weight = ''
-    sound = 'cry-cry'
-    def eat(self, food):
-        self.weight -= food
+    def __init__(self, name, weight, sound='cry-cry'):
+        self.weight = weight
+        self.sound = sound
+        self.name = name
+        def eat(self, food):
+            self.weight -= food
+
+kukareku = Chicken(name='Kukareku',agg=2,weight=12)
+koko = Chicken(name='koko',agg=20,weight=10)
+Grey = Goose(name='Grey',weight=17)
+White = Goose(name='White',weight=19)
+Manka = Cow(name='Manka',weight=144)
+Barashek = Sheep(name='Barashek',weight=46)
+Kudriavy = Sheep(name='Kudriavy',weight=54) 
+Horn = Goat(name='Horn',weight=35)
+Hooves = Goat(name='Hooves',weight=34)
+Kruakva = Duck(name='Kruakva',weight=42)
 
 
-koko = Chicken(agg=20)
 
-print(koko.agg)
- 
+
+print(kukareku.weight + koko.weight + Grey.weight + White.weight + Manka.weight + Barashek.weight + Kudriavy.weight + Horn.weight + Hooves.weight + Kruakva.weight)
+
+
+# listtest = []
+# listtest.append(x)
+# listtest.append(y)
+# listtest.append(z)
+# listtest.sort()
+# print(listtest)
+
+# print(listtest[-1])
